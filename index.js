e3690e80bf86cb30f54bc466572bc6a89d8fd524
Mariano <mariano_aguilar_13@hotmail.com>
2022-11-08 14:12:53 -0300

    Intro-uno-js: Este es el primer ejercicios del intro-javascript

---
+++ index.js
@@ -0,0 +1,15 @@
+function main() {
+  let nameCity = "Baradero";
+  let numRandom = 13;
+  nameCity = numRandom;
+  console.log(nameCity, numRandom);
+
+  const nombre = "Mariano";
+  const apellido = "Aguilar";
+
+  const nombreCompleto = `${nombre} ${apellido}`;
+
+  console.log(nombreCompleto);
+}
+
+main();
